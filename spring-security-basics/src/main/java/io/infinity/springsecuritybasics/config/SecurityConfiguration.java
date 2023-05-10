d0159b543f374a078105f34f05a9d3eae696feff
Anant Bandewar <anant.bandewar@outlook.com>
2023-05-10 11:02:37 +0530

    3. Configured In-Memory Authentication

---
+++ spring-security-basics/src/main/java/io/infinity/springsecuritybasics/config/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package io.infinity.springsecuritybasics.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder.inMemoryAuthentication()
+                .withUser("blah")
+                .password("blah")
+                .roles("USER")
+                .and()
+                .withUser("foo")
+                .password("foo")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
