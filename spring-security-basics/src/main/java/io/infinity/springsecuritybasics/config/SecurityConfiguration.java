4788d1f9fe0140003665b78f6cd56d0b301d6f25
Anant Bandewar <anant.bandewar@outlook.com>
2023-05-10 12:39:28 +0530

    5. Implemented JDBC authentication with default schema using H2 DB

--- spring-security-basics/src/main/java/io/infinity/springsecuritybasics/config/SecurityConfiguration.java
+++ spring-security-basics/src/main/java/io/infinity/springsecuritybasics/config/SecurityConfiguration.java
@@ -1,18 +1,29 @@
 package io.infinity.springsecuritybasics.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+
+        // In-Memory Authentication
+        /*
         builder.inMemoryAuthentication()
                 .withUser("blah")
                 .password("blah")
@@ -21,6 +32,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .withUser("foo")
                 .password("foo")
                 .roles("ADMIN");
+        */
+
+
+        // JDBC Authentication
+        builder.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("foo")
+                                .password("foo")
+                                .roles("ADMIN")
+                )
+                .withUser(
+                        User.withUsername("blah")
+                                .password("blah")
+                                .roles("USER")
+                );
     }
 
     @Override
